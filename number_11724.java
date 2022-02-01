7cc9c8d96bc417509b706af4d60d7152ffbd4788
JUNGWOO JANG <jw1010110@gmail.com>
2022-02-02 00:27:38 +0900

    Initial commit

--- number_11724.java
+++ number_11724.java
@@ -3,25 +3,30 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class number_11724 {
+    
     static LinkedList<Integer> ajlist[];
     static Boolean visit[];
     static int count=0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         ajlist = new LinkedList[n+1];
         visit = new Boolean[n+1];
         Arrays.fill(visit, false);
+
         for(int i=0;i<n+1;i++){
             ajlist[i] = new LinkedList<>();
         }
+
         for(int i=0;i<m;i++){
             int from = sc.nextInt();
             int to = sc.nextInt();
             ajlist[from].add(to);
             ajlist[to].add(from);
         }sc.close();
+
         for(int i=1;i<n+1;i++){
             if(DFS(i)){
                 count+=1;
